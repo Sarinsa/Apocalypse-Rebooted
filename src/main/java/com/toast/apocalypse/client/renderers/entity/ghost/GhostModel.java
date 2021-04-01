2d147167c1bc783a2db8490956d87409303a882b
Sarinsa <mikkemusbruker@gmail.com>
2021-04-01 14:03:17 +0200

    Undid botched attempt to get the ghost to render with transparency.

--- src/main/java/com/toast/apocalypse/client/renderers/entity/ghost/GhostModel.java
+++ src/main/java/com/toast/apocalypse/client/renderers/entity/ghost/GhostModel.java
@@ -9,17 +9,17 @@ import net.minecraft.client.renderer.model.ModelRenderer;
 
 public class GhostModel<T extends GhostEntity> extends SegmentedModel<T> {
 
-    private ModelRenderer head;
-    private ModelRenderer body;
-    private ModelRenderer rightArm;
-    private ModelRenderer leftArm;
+    private final ModelRenderer head;
+    private final ModelRenderer body;
+    private final ModelRenderer rightArm;
+    private final ModelRenderer leftArm;
 
     public GhostModel() {
-        this.head = new TransparentModelRenderer(this, 0, 0).addBox(-2.0F, -10.0F, -2.0F, 4, 10, 4);
-        this.body = new TransparentModelRenderer(this, 16, 10).addBox(-4.0F, 0.0F, -2.0F, 8, 18, 4);
-        this.rightArm = new TransparentModelRenderer(this, 0, 16).addBox(-3.0F, -2.0F, -2.0F, 4, 12, 4);
+        this.head = new ModelRenderer(this, 0, 0).addBox(-2.0F, -10.0F, -2.0F, 4, 10, 4);
+        this.body = new ModelRenderer(this, 16, 10).addBox(-4.0F, 0.0F, -2.0F, 8, 18, 4);
+        this.rightArm = new ModelRenderer(this, 0, 16).addBox(-3.0F, -2.0F, -2.0F, 4, 12, 4);
         this.rightArm.setPos(-5.0F, 2.0F, 0.0F);
-        this.leftArm = new TransparentModelRenderer(this, 0, 16).addBox(-1.0F, -2.0F, -2.0F, 4, 12, 4);
+        this.leftArm = new ModelRenderer(this, 0, 16).addBox(-1.0F, -2.0F, -2.0F, 4, 12, 4);
         this.leftArm.setPos(5.0F, 2.0F, 0.0F);
         this.leftArm.mirror = true;
     }
