4d40a0e3dedfdb6644933c3744c72041fe809a25
Sarinsa <mikkemusbruker@gmail.com>
2021-04-01 12:34:58 +0200

    Created a helper method for easily creating shapeless recipes.

--- src/main/java/com/toast/apocalypse/datagen/ApocalypseRecipeProvider.java
+++ src/main/java/com/toast/apocalypse/datagen/ApocalypseRecipeProvider.java
@@ -6,7 +6,9 @@ import net.minecraft.data.IFinishedRecipe;
 import net.minecraft.data.RecipeProvider;
 import net.minecraft.data.ShapelessRecipeBuilder;
 import net.minecraft.item.Items;
+import net.minecraft.util.IItemProvider;
 
+import java.util.Objects;
 import java.util.function.Consumer;
 
 public class ApocalypseRecipeProvider extends RecipeProvider {
@@ -17,9 +19,17 @@ public class ApocalypseRecipeProvider extends RecipeProvider {
 
     @Override
     protected void buildShapelessRecipes(Consumer<IFinishedRecipe> consumer) {
-        ShapelessRecipeBuilder.shapeless(ApocalypseItems.BUCKET_HELM.get(), 1)
-                .requires(Items.BUCKET)
-                .unlockedBy("has_bucket", has(Items.BUCKET))
-                .save(consumer);
+        this.simpleShapelessRecipe(ApocalypseItems.BUCKET_HELM.get(), 1, consumer, Items.BUCKET);
+    }
+
+    private void simpleShapelessRecipe(IItemProvider result, int count, Consumer<IFinishedRecipe> consumer, IItemProvider... ingredients) {
+        ShapelessRecipeBuilder builder = ShapelessRecipeBuilder.shapeless(result, count);
+
+        for (IItemProvider ingredient : ingredients) {
+            builder.requires(ingredient);
+            String ingredientName = Objects.requireNonNull(ingredient.asItem().getRegistryName()).getPath();
+            builder.unlockedBy("has_" + ingredientName, has(ingredient));
+        }
+        builder.save(consumer);
     }
 }
