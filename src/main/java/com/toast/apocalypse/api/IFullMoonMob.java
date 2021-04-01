cd6d98a9d0b74acbe036a000cdf9b7c025916f2b
Sarinsa <mikkemusbruker@gmail.com>
2021-04-01 14:04:04 +0200

    Added default method for deciding if a full moon monster should never despawn during full moons.

--- src/main/java/com/toast/apocalypse/api/IFullMoonMob.java
+++ src/main/java/com/toast/apocalypse/api/IFullMoonMob.java
@@ -4,4 +4,15 @@ package com.toast.apocalypse.api;
  * This represents a type of monster that will only spawn as part of a full moon event.
  */
 public interface IFullMoonMob {
+
+    /**
+     * Determines whether or not a full moon
+     * monster should be prevented from
+     * despawning during full moons.
+     *
+     * @return True if this entity should not despawn.
+     */
+    default boolean persistentDuringFullMoon() {
+        return true;
+    }
 }
