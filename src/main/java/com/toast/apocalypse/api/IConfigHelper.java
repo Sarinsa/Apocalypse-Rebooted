c4b9992b6407d5199a6c84090522688df4ce177f
Sarinsa <mikkemusbruker@gmail.com>
2021-04-07 22:55:48 +0200

    Added a helper method to IConfigHelper to fetch the mod config's rain damage enabled value

--- src/main/java/com/toast/apocalypse/api/IConfigHelper.java
+++ src/main/java/com/toast/apocalypse/api/IConfigHelper.java
@@ -6,4 +6,10 @@ package com.toast.apocalypse.api;
  * settings.
  */
 public interface IConfigHelper {
+
+    /**
+     * @return True if rain damage is
+     *         enabled in the mod config.
+     */
+    boolean rainDamageEnabled();
 }
--- src/main/java/com/toast/apocalypse/api/impl/ConfigHelper.java
+++ src/main/java/com/toast/apocalypse/api/impl/ConfigHelper.java
@@ -1,6 +1,17 @@
 package com.toast.apocalypse.api.impl;
 
 import com.toast.apocalypse.api.IConfigHelper;
+import com.toast.apocalypse.common.core.config.ApocalypseCommonConfig;
 
 public final class ConfigHelper implements IConfigHelper {
+
+
+    public ConfigHelper() {
+
+    }
+
+    @Override
+    public boolean rainDamageEnabled() {
+        return ApocalypseCommonConfig.COMMON.rainDamageEnabled();
+    }
 }
