ef2dafaa59f582d0b0d6bfc67d05774e930d1be4
Sarinsa <mikkemusbruker@gmail.com>
2021-04-08 22:54:55 +0200

    Fixed the set difficulty command.

--- src/main/java/com/toast/apocalypse/common/command/ApocalypseBaseCommand.java
+++ src/main/java/com/toast/apocalypse/common/command/ApocalypseBaseCommand.java
@@ -9,10 +9,12 @@ import com.toast.apocalypse.common.network.NetworkHelper;
 import com.toast.apocalypse.common.util.References;
 import net.minecraft.command.CommandSource;
 import net.minecraft.command.Commands;
+import net.minecraft.command.impl.TimeCommand;
 import net.minecraft.util.Util;
 import net.minecraft.util.text.TranslationTextComponent;
 
 public class ApocalypseBaseCommand {
+
 
     public static void register(CommandDispatcher<CommandSource> dispatcher) {
         dispatcher.register(Commands.literal("apocalypse")
@@ -33,8 +35,8 @@ public class ApocalypseBaseCommand {
         private static ArgumentBuilder<CommandSource, ?> register() {
             return Commands.literal("set")
                     .requires((source) -> source.hasPermission(3))
-                    .then(Commands.argument("difficulty", DifficultyArgument.difficulty()))
-                    .executes((context) -> setWorldDifficulty(context.getSource(), LongArgumentType.getLong(context, "difficulty")));
+                    .then(Commands.argument("difficulty", DifficultyArgument.difficulty())
+                    .executes((context) -> setWorldDifficulty(context.getSource(), LongArgumentType.getLong(context, "difficulty"))));
         }
 
         private static int setWorldDifficulty(CommandSource source, long difficulty) {
