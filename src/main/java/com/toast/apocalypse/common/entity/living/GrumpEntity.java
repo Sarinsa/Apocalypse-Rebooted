f3f9a0957ddb74e6e36bd34a2cabcd4f5676e72a
Sarinsa <mikkemusbruker@gmail.com>
2021-04-10 01:26:24 +0200

    Groump

--- src/main/java/com/toast/apocalypse/common/entity/living/GrumpEntity.java
+++ src/main/java/com/toast/apocalypse/common/entity/living/GrumpEntity.java
@@ -3,16 +3,22 @@ package com.toast.apocalypse.common.entity.living;
 import net.minecraft.entity.*;
 import net.minecraft.entity.ai.attributes.AttributeModifierMap;
 import net.minecraft.entity.ai.attributes.Attributes;
+import net.minecraft.entity.ai.goal.Goal;
+import net.minecraft.entity.ai.goal.MeleeAttackGoal;
 import net.minecraft.entity.ai.goal.NearestAttackableTargetGoal;
 import net.minecraft.entity.monster.GhastEntity;
 import net.minecraft.entity.monster.IMob;
+import net.minecraft.entity.monster.VexEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.world.Difficulty;
 import net.minecraft.world.IServerWorld;
 import net.minecraft.world.World;
 
+import java.util.EnumSet;
 import java.util.Random;
 
 /**
@@ -38,7 +44,7 @@ public class GrumpEntity extends GhastEntity implements IMob {
 
     @Override
     protected void registerGoals() {
-
+        this.goalSelector.addGoal(0, new GrumpEntity.MeleeAttackGoal(this));
         this.targetSelector.addGoal(1, new NearestAttackableTargetGoal<>(this, PlayerEntity.class, 10, true, false, (p_213812_1_) -> {
             return Math.abs(p_213812_1_.getY() - this.getY()) <= 4.0D;
         }));
@@ -61,5 +67,83 @@ public class GrumpEntity extends GhastEntity implements IMob {
 
     public static boolean checkGrumpSpawnRules(EntityType<? extends GrumpEntity> entityType, IServerWorld world, SpawnReason spawnReason, BlockPos pos, Random random) {
         return world.getDifficulty() != Difficulty.PEACEFUL && MobEntity.checkMobSpawnRules(entityType, world, spawnReason, pos, random);
+    }
+
+    /** Copied from ghast */
+    static class LookAroundGoal extends Goal {
+        private final GrumpEntity grump;
+
+        public LookAroundGoal(GrumpEntity grump) {
+            this.grump = grump;
+            this.setFlags(EnumSet.of(Goal.Flag.LOOK));
+        }
+
+        @Override
+        public boolean canUse() {
+            return true;
+        }
+
+        public void tick() {
+            if (this.grump.getTarget() == null) {
+                Vector3d vector3d = this.grump.getDeltaMovement();
+                this.grump.yRot = -((float) MathHelper.atan2(vector3d.x, vector3d.z)) * (180F / (float)Math.PI);
+            } else {
+                LivingEntity target = this.grump.getTarget();
+
+                double x = target.getX() - this.grump.getX();
+                double z = target.getZ() - this.grump.getZ();
+                this.grump.yRot = -((float)MathHelper.atan2(x, z)) * (180F / (float)Math.PI);
+            }
+            this.grump.yBodyRot = this.grump.yRot;
+        }
+    }
+
+    private static class MeleeAttackGoal extends Goal {
+
+        final GrumpEntity grump;
+
+        public MeleeAttackGoal(GrumpEntity grump) {
+            this.setFlags(EnumSet.of(Goal.Flag.MOVE));
+            this.grump = grump;
+        }
+
+        private void setWantedPosition(LivingEntity target) {
+            Vector3d vector = target.getEyePosition(1.0F).add(0.0D, -(this.grump.getBbHeight() / 1.8), 0.0D);
+            this.grump.moveControl.setWantedPosition(vector.x, vector.y, vector.z, 1.0D);
+        }
+
+        @Override
+        public boolean canUse() {
+            return this.grump.getTarget() != null && !this.grump.getMoveControl().hasWanted();
+        }
+
+        @Override
+        public boolean canContinueToUse() {
+            return this.grump.getMoveControl().hasWanted() && this.grump.getTarget() != null && this.grump.getTarget().isAlive();
+        }
+
+        @Override
+        public void start() {
+            LivingEntity target = this.grump.getTarget();
+
+            if (target != null)
+                this.setWantedPosition(target);
+        }
+
+        @Override
+        public void stop() {
+        }
+
+        @Override
+        public void tick() {
+            LivingEntity target = this.grump.getTarget();
+
+            if (this.grump.getBoundingBox().intersects(target.getBoundingBox())) {
+                this.grump.doHurtTarget(target);
+            }
+            else {
+                this.setWantedPosition(target);
+            }
+        }
     }
 }
