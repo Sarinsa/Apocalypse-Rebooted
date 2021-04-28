321314e17ad291d108b2a495f3554a1359b294ec
Sarinsa <mikkemusbruker@gmail.com>
2021-04-29 00:04:50 +0200

    Augh

--- src/main/java/com/toast/apocalypse/api/TimeChangedEvent.java
+++ src/main/java/com/toast/apocalypse/api/TimeChangedEvent.java
@@ -2,19 +2,28 @@ package com.toast.apocalypse.api;
 
 import net.minecraftforge.eventbus.api.Event;
 
+/**
+ * This event can be fired to notify Apocalypse of
+ * a change in world time in order to update difficulty
+ * accordingly. Intended usage is for in-game events,
+ * such as when players sleep (the time set command is excluded)
+ */
 public final class TimeChangedEvent extends Event {
 
-    private long newTime;
+    private long timeSkipped;
 
-    public TimeChangedEvent(long newTime) {
-        this.newTime = newTime;
+    public TimeChangedEvent(long timeSkipped) {
+        this.timeSkipped = timeSkipped;
     }
 
-    public void setNewTime(long newTime) {
-        this.newTime = newTime;
+    /**
+     * @param timeSkipped The amount of time skipped.
+     */
+    public void setTimeSkipped(long timeSkipped) {
+        this.timeSkipped = timeSkipped;
     }
 
-    public long getNewTime() {
-        return this.newTime;
+    public long getTimeSkipped() {
+        return this.timeSkipped;
     }
 }
