0fb55acd92de4189b2763a552bcb4d379bf93bab
Sarinsa <unconfigured@null.spigotmc.org>
2021-06-29 03:50:53 +0200

    Added new recipes for Lunar Clock, Moon Phase Sensor and Lunarium Ingot + updated full moon mob loot tables.

--- src/main/java/com/toast/apocalypse/datagen/ApocalypseEntityLootTableProvider.java
+++ src/main/java/com/toast/apocalypse/datagen/ApocalypseEntityLootTableProvider.java
@@ -1,12 +1,15 @@
 package com.toast.apocalypse.datagen;
 
 import com.toast.apocalypse.common.register.ApocalypseEntities;
+import com.toast.apocalypse.common.register.ApocalypseItems;
 import net.minecraft.data.loot.EntityLootTables;
 import net.minecraft.entity.EntityType;
 import net.minecraft.item.Items;
 import net.minecraft.loot.*;
+import net.minecraft.loot.conditions.EntityHasProperty;
 import net.minecraft.loot.functions.LootingEnchantBonus;
 import net.minecraft.loot.functions.SetCount;
+import net.minecraft.loot.functions.Smelt;
 
 import java.util.HashSet;
 import java.util.Random;
@@ -37,7 +40,7 @@ public class ApocalypseEntityLootTableProvider extends EntityLootTables {
                                 .apply(LootingEnchantBonus.lootingMultiplier(RandomValueRange.between(0.0F, 2.0F)))))
                 .withPool(LootPool.lootPool()
                         .setRolls(ConstantRange.exactly(1))
-                        .add(ItemLootEntry.lootTableItem(Items.SLIME_BALL)
+                        .add(ItemLootEntry.lootTableItem(ApocalypseItems.SOUL_FRAGMENT.get())
                                 .apply(SetCount.setCount(RandomValueRange.between(0.0F, 2.0F)))
                                 .apply(LootingEnchantBonus.lootingMultiplier(RandomValueRange.between(0.0F, 2.0F))))));
 
@@ -46,9 +49,47 @@ public class ApocalypseEntityLootTableProvider extends EntityLootTables {
                         .setRolls(ConstantRange.exactly(1))
                         .add(ItemLootEntry.lootTableItem(Items.COOKIE)
                                 .apply(SetCount.setCount(RandomValueRange.between(0.0F, 3.0F)))
-                                .apply(LootingEnchantBonus.lootingMultiplier(RandomValueRange.between(0.0F, 2.0F))))
-                        .add(ItemLootEntry.lootTableItem(Items.EXPERIENCE_BOTTLE)
-                                .apply(SetCount.setCount(RandomValueRange.between(0.0F, 0.2F)))
+                                .apply(LootingEnchantBonus.lootingMultiplier(RandomValueRange.between(0.0F, 2.0F)))))
+                .withPool(LootPool.lootPool()
+                        .setRolls(ConstantRange.exactly(1))
+                        .add(ItemLootEntry.lootTableItem(ApocalypseItems.SOUL_FRAGMENT.get())
+                                .apply(SetCount.setCount(RandomValueRange.between(0.0F, 2.0F)))
+                                .apply(LootingEnchantBonus.lootingMultiplier(RandomValueRange.between(0.0F, 2.0F))))));
+
+        this.add(ApocalypseEntities.SEEKER.get(), LootTable.lootTable()
+                .withPool(LootPool.lootPool()
+                        .setRolls(ConstantRange.exactly(1))
+                        .add(ItemLootEntry.lootTableItem(Items.GUNPOWDER)
+                                .apply(SetCount.setCount(RandomValueRange.between(1.0F, 3.0F)))
+                                .apply(LootingEnchantBonus.lootingMultiplier(RandomValueRange.between(0.0F, 2.0F)))))
+                .withPool(LootPool.lootPool()
+                        .setRolls(ConstantRange.exactly(1))
+                        .add(ItemLootEntry.lootTableItem(ApocalypseItems.SOUL_FRAGMENT.get())
+                                .apply(SetCount.setCount(RandomValueRange.between(0.0F, 2.0F)))
+                                .apply(LootingEnchantBonus.lootingMultiplier(RandomValueRange.between(0.0F, 2.0F))))));
+
+        this.add(ApocalypseEntities.DESTROYER.get(), LootTable.lootTable()
+                .withPool(LootPool.lootPool()
+                        .setRolls(ConstantRange.exactly(1))
+                        .add(ItemLootEntry.lootTableItem(Items.GUNPOWDER)
+                                .apply(SetCount.setCount(RandomValueRange.between(1.0F, 5.0F)))
+                                .apply(LootingEnchantBonus.lootingMultiplier(RandomValueRange.between(0.0F, 2.0F)))))
+                .withPool(LootPool.lootPool()
+                        .setRolls(ConstantRange.exactly(1))
+                        .add(ItemLootEntry.lootTableItem(ApocalypseItems.SOUL_FRAGMENT.get())
+                                .apply(SetCount.setCount(RandomValueRange.between(0.0F, 2.0F)))
+                                .apply(LootingEnchantBonus.lootingMultiplier(RandomValueRange.between(0.0F, 2.0F))))));
+
+        this.add(ApocalypseEntities.BREECHER.get(), LootTable.lootTable()
+                .withPool(LootPool.lootPool()
+                        .setRolls(ConstantRange.exactly(1))
+                        .add(ItemLootEntry.lootTableItem(Items.GUNPOWDER)
+                                .apply(SetCount.setCount(RandomValueRange.between(1.0F, 4.0F)))
+                                .apply(LootingEnchantBonus.lootingMultiplier(RandomValueRange.between(0.0F, 2.0F)))))
+                .withPool(LootPool.lootPool()
+                        .setRolls(ConstantRange.exactly(1))
+                        .add(ItemLootEntry.lootTableItem(ApocalypseItems.SOUL_FRAGMENT.get())
+                                .apply(SetCount.setCount(RandomValueRange.between(0.0F, 2.0F)))
                                 .apply(LootingEnchantBonus.lootingMultiplier(RandomValueRange.between(0.0F, 2.0F))))));
     }
 }
--- src/main/java/com/toast/apocalypse/datagen/ApocalypseRecipeProvider.java
+++ src/main/java/com/toast/apocalypse/datagen/ApocalypseRecipeProvider.java
@@ -1,12 +1,12 @@
 package com.toast.apocalypse.datagen;
 
+import com.toast.apocalypse.common.register.ApocalypseBlocks;
 import com.toast.apocalypse.common.register.ApocalypseItems;
-import net.minecraft.data.DataGenerator;
-import net.minecraft.data.IFinishedRecipe;
-import net.minecraft.data.RecipeProvider;
-import net.minecraft.data.ShapelessRecipeBuilder;
+import net.minecraft.data.*;
 import net.minecraft.item.Items;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.util.IItemProvider;
+import net.minecraftforge.common.Tags;
 
 import java.util.Objects;
 import java.util.function.Consumer;
@@ -20,10 +20,35 @@ public class ApocalypseRecipeProvider extends RecipeProvider {
     @Override
     protected void buildShapelessRecipes(Consumer<IFinishedRecipe> consumer) {
         this.simpleShapelessRecipe(ApocalypseItems.BUCKET_HELM.get(), 1, consumer, Items.BUCKET);
+
+        ShapelessRecipeBuilder.shapeless(ApocalypseItems.LUNARIUM_INGOT.get(), 1)
+                .requires(Tags.Items.INGOTS_IRON)
+                .requires(ApocalypseItems.SOUL_FRAGMENT.get())
+                .unlockedBy("has_" + Objects.requireNonNull(ApocalypseItems.SOUL_FRAGMENT.get().asItem().getRegistryName()).getPath(), has(ApocalypseItems.SOUL_FRAGMENT.get()))
+                .save(consumer);
+
+        ShapedRecipeBuilder.shaped(ApocalypseBlocks.LUNAR_PHASE_SENSOR.get().asItem(), 1)
+                .pattern("###")
+                .pattern("LLL")
+                .pattern("SSS")
+                .define('#', Tags.Items.GLASS)
+                .define('L', ApocalypseItems.LUNARIUM_INGOT.get())
+                .define('S', ItemTags.WOODEN_SLABS)
+                .unlockedBy("has_" + ApocalypseItems.LUNARIUM_INGOT.get().getRegistryName().getPath(), has(ApocalypseItems.LUNARIUM_INGOT.get()))
+                .save(consumer);
+
+        ShapedRecipeBuilder.shaped(ApocalypseItems.LUNAR_CLOCK.get(), 1)
+                .pattern(" # ")
+                .pattern("#R#")
+                .pattern(" # ")
+                .define('#', ApocalypseItems.LUNARIUM_INGOT.get())
+                .define('R', Tags.Items.DUSTS_REDSTONE)
+                .unlockedBy("has_" + ApocalypseItems.LUNARIUM_INGOT.get().getRegistryName().getPath(), has(ApocalypseItems.LUNARIUM_INGOT.get()))
+                .save(consumer);
     }
 
     private void simpleShapelessRecipe(IItemProvider result, int count, Consumer<IFinishedRecipe> consumer, IItemProvider... ingredients) {
-        ShapelessRecipeBuilder builder = ShapelessRecipeBuilder.shapeless(result, count);
+        final ShapelessRecipeBuilder builder = ShapelessRecipeBuilder.shapeless(result, count);
 
         for (IItemProvider ingredient : ingredients) {
             builder.requires(ingredient);
