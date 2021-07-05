b4cc0266f0209bc3cc8ec3481125ace37b82e45f
Sarinsa <mikkemusbruker@gmail.com>
2021-07-05 22:18:28 +0200

    Fixed invalid method javadoc reference.

--- src/main/java/com/toast/apocalypse/common/util/RainDamageTickHelper.java
+++ src/main/java/com/toast/apocalypse/common/util/RainDamageTickHelper.java
@@ -27,7 +27,7 @@ public class RainDamageTickHelper {
      * Checks if it is time to apply rain tick damage,
      * and applies the damage to the player if so.
      *
-     * @see com.toast.apocalypse.common.event.EntityEvents#onPlayerTick(TickEvent.PlayerTickEvent)
+     * @see com.toast.apocalypse.common.event.PlayerEvents#onPlayerTick(TickEvent.PlayerTickEvent)
      */
     public static void checkAndPerformRainDamageTick(PlayerEntity player) {
         World world = player.getCommandSenderWorld();
