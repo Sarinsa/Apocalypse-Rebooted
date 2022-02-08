980041b6a28dbdae941bcc9ab30119c6d9714f56
Sarinsa <mikkemusbruker@gmail.com>
2022-02-08 23:28:19 +0100

    Fixed Seeker mob alerting not working + tweaked SeekerAlertEvent to be more flexible

--- src/main/java/com/toast/apocalypse/api/SeekerAlertEvent.java
+++ src/main/java/com/toast/apocalypse/api/SeekerAlertEvent.java
@@ -8,32 +8,34 @@ import net.minecraftforge.eventbus.api.Cancelable;
 import net.minecraftforge.eventbus.api.Event;
 
 import javax.annotation.Nonnull;
+import java.util.List;
 
 /**
- * Fired when a Seeker attempts to alert another entity of its target player.<br>
+ * Fired when a Seeker attempts to alert nearby entities of its target player.<br>
  * <br>
  *
  * This event is fired from {@link SeekerEntity.AlertOtherMonstersGoal#start()}<br>
  * <br>
  *
  * {@link #seeker} is the Seeker entity that is trying to alert an entity.<br>
- * {@link #toAlert} is the MobEntity the Seeker is trying to alert.<br>
+ * {@link #toAlert} is a List of mob entities the Seeker is trying to alert.<br>
  * {@link #target} is the Seeker's current target, usually a player.<br>
  * {@link #world} is the Seeker's World object.<br>
  * <br>
  *
- * This event is {@link Cancelable}<br>
+ * This event is not {@link Cancelable}<br>
+ * <br>
+ * The {@link #toAlert} List is modifiable, and mobs can be both added and removed.<br>
  * <br>
  */
-@Cancelable
 public final class SeekerAlertEvent extends Event {
 
     private final MobEntity seeker;
-    private final MobEntity toAlert;
+    private final List<MobEntity> toAlert;
     private final LivingEntity target;
     private final World world;
 
-    public SeekerAlertEvent(World world, @Nonnull MobEntity seeker, @Nonnull MobEntity toAlert, @Nonnull LivingEntity target) {
+    public SeekerAlertEvent(World world, @Nonnull MobEntity seeker, @Nonnull List<MobEntity> toAlert, @Nonnull LivingEntity target) {
         this.seeker = seeker;
         this.toAlert = toAlert;
         this.target = target;
@@ -44,7 +46,7 @@ public final class SeekerAlertEvent extends Event {
         return this.seeker;
     }
 
-    public MobEntity getToAlert() {
+    public List<MobEntity> getToAlert() {
         return this.toAlert;
     }
 
