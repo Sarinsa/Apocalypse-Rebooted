254a2ceda78b688540a7ed5a69eeec56fcf4239d
Sarinsa <mikkemusbruker@gmail.com>
2022-12-27 19:08:31 +0100

    Cleaned up FearwolfModel

--- src/main/java/com/toast/apocalypse/client/renderer/entity/living/fearwolf/FearwolfModel.java
+++ src/main/java/com/toast/apocalypse/client/renderer/entity/living/fearwolf/FearwolfModel.java
@@ -2,14 +2,14 @@ package com.toast.apocalypse.client.renderer.entity.living.fearwolf;
 
 import com.google.common.collect.ImmutableList;
 import com.toast.apocalypse.common.entity.living.FearwolfEntity;
-import net.minecraft.client.renderer.entity.model.TintedAgeableModel;
+import net.minecraft.client.renderer.entity.model.AgeableModel;
 import net.minecraft.client.renderer.model.ModelRenderer;
 import net.minecraft.util.math.MathHelper;
 
 /**
  * Basically a copy of the vanilla WolfModel.
  */
-public class FearwolfModel <T extends FearwolfEntity> extends TintedAgeableModel<T> {
+public class FearwolfModel <T extends FearwolfEntity> extends AgeableModel<T> {
 
     private final ModelRenderer head;
     private final ModelRenderer realHead;
@@ -23,65 +23,65 @@ public class FearwolfModel <T extends FearwolfEntity> extends TintedAgeableModel
     private final ModelRenderer upperBody;
 
     public FearwolfModel() {
-        this.head = new ModelRenderer(this, 0, 0);
-        this.head.setPos(-1.0F, 13.5F, -7.0F);
-        this.realHead = new ModelRenderer(this, 0, 0);
-        this.realHead.addBox(-2.0F, -3.0F, -2.0F, 6.0F, 6.0F, 4.0F, 0.0F);
-        this.head.addChild(this.realHead);
-        this.body = new ModelRenderer(this, 18, 14);
-        this.body.addBox(-3.0F, -2.0F, -3.0F, 6.0F, 9.0F, 6.0F, 0.0F);
-        this.body.setPos(0.0F, 14.0F, 2.0F);
-        this.upperBody = new ModelRenderer(this, 21, 0);
-        this.upperBody.addBox(-3.0F, -3.0F, -3.0F, 8.0F, 6.0F, 7.0F, 0.0F);
-        this.upperBody.setPos(-1.0F, 14.0F, 2.0F);
-        this.leg0 = new ModelRenderer(this, 0, 18);
-        this.leg0.addBox(0.0F, 0.0F, -1.0F, 2.0F, 8.0F, 2.0F, 0.0F);
-        this.leg0.setPos(-2.5F, 16.0F, 7.0F);
-        this.leg1 = new ModelRenderer(this, 0, 18);
-        this.leg1.addBox(0.0F, 0.0F, -1.0F, 2.0F, 8.0F, 2.0F, 0.0F);
-        this.leg1.setPos(0.5F, 16.0F, 7.0F);
-        this.leg2 = new ModelRenderer(this, 0, 18);
-        this.leg2.addBox(0.0F, 0.0F, -1.0F, 2.0F, 8.0F, 2.0F, 0.0F);
-        this.leg2.setPos(-2.5F, 16.0F, -4.0F);
-        this.leg3 = new ModelRenderer(this, 0, 18);
-        this.leg3.addBox(0.0F, 0.0F, -1.0F, 2.0F, 8.0F, 2.0F, 0.0F);
-        this.leg3.setPos(0.5F, 16.0F, -4.0F);
-        this.tail = new ModelRenderer(this, 9, 18);
-        this.tail.setPos(-1.0F, 12.0F, 8.0F);
-        this.realTail = new ModelRenderer(this, 9, 18);
-        this.realTail.addBox(0.0F, 0.0F, -1.0F, 2.0F, 8.0F, 2.0F, 0.0F);
-        this.tail.addChild(this.realTail);
-        this.realHead.texOffs(16, 14).addBox(-2.0F, -5.0F, 0.0F, 2.0F, 2.0F, 1.0F, 0.0F);
-        this.realHead.texOffs(16, 14).addBox(2.0F, -5.0F, 0.0F, 2.0F, 2.0F, 1.0F, 0.0F);
-        this.realHead.texOffs(0, 10).addBox(-0.5F, 0.0F, -5.0F, 3.0F, 3.0F, 4.0F, 0.0F);
+        head = new ModelRenderer(this, 0, 0);
+        head.setPos(-1.0F, 13.5F, -7.0F);
+        realHead = new ModelRenderer(this, 0, 0);
+        realHead.addBox(-2.0F, -3.0F, -2.0F, 6.0F, 6.0F, 4.0F, 0.0F);
+        head.addChild(realHead);
+        body = new ModelRenderer(this, 18, 14);
+        body.addBox(-3.0F, -2.0F, -3.0F, 6.0F, 9.0F, 6.0F, 0.0F);
+        body.setPos(0.0F, 14.0F, 2.0F);
+        upperBody = new ModelRenderer(this, 21, 0);
+        upperBody.addBox(-3.0F, -3.0F, -3.0F, 8.0F, 6.0F, 7.0F, 0.0F);
+        upperBody.setPos(-1.0F, 14.0F, 2.0F);
+        leg0 = new ModelRenderer(this, 0, 18);
+        leg0.addBox(0.0F, 0.0F, -1.0F, 2.0F, 8.0F, 2.0F, 0.0F);
+        leg0.setPos(-2.5F, 16.0F, 7.0F);
+        leg1 = new ModelRenderer(this, 0, 18);
+        leg1.addBox(0.0F, 0.0F, -1.0F, 2.0F, 8.0F, 2.0F, 0.0F);
+        leg1.setPos(0.5F, 16.0F, 7.0F);
+        leg2 = new ModelRenderer(this, 0, 18);
+        leg2.addBox(0.0F, 0.0F, -1.0F, 2.0F, 8.0F, 2.0F, 0.0F);
+        leg2.setPos(-2.5F, 16.0F, -4.0F);
+        leg3 = new ModelRenderer(this, 0, 18);
+        leg3.addBox(0.0F, 0.0F, -1.0F, 2.0F, 8.0F, 2.0F, 0.0F);
+        leg3.setPos(0.5F, 16.0F, -4.0F);
+        tail = new ModelRenderer(this, 9, 18);
+        tail.setPos(-1.0F, 12.0F, 8.0F);
+        realTail = new ModelRenderer(this, 9, 18);
+        realTail.addBox(0.0F, 0.0F, -1.0F, 2.0F, 8.0F, 2.0F, 0.0F);
+        tail.addChild(this.realTail);
+        realHead.texOffs(16, 14).addBox(-2.0F, -5.0F, 0.0F, 2.0F, 2.0F, 1.0F, 0.0F);
+        realHead.texOffs(16, 14).addBox(2.0F, -5.0F, 0.0F, 2.0F, 2.0F, 1.0F, 0.0F);
+        realHead.texOffs(0, 10).addBox(-0.5F, 0.0F, -5.0F, 3.0F, 3.0F, 4.0F, 0.0F);
     }
 
     @Override
     protected Iterable<ModelRenderer> headParts() {
-        return ImmutableList.of(this.head);
+        return ImmutableList.of(head);
     }
 
     @Override
     protected Iterable<ModelRenderer> bodyParts() {
-        return ImmutableList.of(this.body, this.leg0, this.leg1, this.leg2, this.leg3, this.tail, this.upperBody);
+        return ImmutableList.of(body, leg0, leg1, leg2, leg3, tail, upperBody);
     }
 
     @Override
     public void prepareMobModel(T fearwolf, float p_212843_2_, float p_212843_3_, float p_212843_4_) {
-        this.body.setPos(0.0F, 14.0F, 2.0F);
-        this.body.xRot = ((float) Math.PI / 2F);
-        this.upperBody.setPos(-1.0F, 14.0F, -3.0F);
-        this.upperBody.xRot = this.body.xRot;
-        this.tail.setPos(-1.0F, 12.0F, 8.0F);
-        this.leg0.setPos(-2.5F, 16.0F, 7.0F);
-        this.leg1.setPos(0.5F, 16.0F, 7.0F);
-        this.leg2.setPos(-2.5F, 16.0F, -4.0F);
-        this.leg3.setPos(0.5F, 16.0F, -4.0F);
-        this.leg0.xRot = MathHelper.cos(p_212843_2_ * 0.4662F) * 1.1F * p_212843_3_;
-        this.leg1.xRot = MathHelper.cos(p_212843_2_ * 0.4662F + (float) Math.PI) * 1.1F * p_212843_3_;
-        this.leg2.xRot = MathHelper.cos(p_212843_2_ * 0.4662F + (float) Math.PI) * 1.1F * p_212843_3_;
-        this.leg3.xRot = MathHelper.cos(p_212843_2_ * 0.4662F) * 1.1F * p_212843_3_;
-        this.tail.xRot = -35.0F;
+        body.setPos(0.0F, 14.0F, 2.0F);
+        body.xRot = ((float) Math.PI / 2F);
+        upperBody.setPos(-1.0F, 14.0F, -3.0F);
+        upperBody.xRot = body.xRot;
+        tail.setPos(-1.0F, 12.0F, 8.0F);
+        leg0.setPos(-2.5F, 16.0F, 7.0F);
+        leg1.setPos(0.5F, 16.0F, 7.0F);
+        leg2.setPos(-2.5F, 16.0F, -4.0F);
+        leg3.setPos(0.5F, 16.0F, -4.0F);
+        leg0.xRot = MathHelper.cos(p_212843_2_ * 0.4662F) * 1.1F * p_212843_3_;
+        leg1.xRot = MathHelper.cos(p_212843_2_ * 0.4662F + (float) Math.PI) * 1.1F * p_212843_3_;
+        leg2.xRot = MathHelper.cos(p_212843_2_ * 0.4662F + (float) Math.PI) * 1.1F * p_212843_3_;
+        leg3.xRot = MathHelper.cos(p_212843_2_ * 0.4662F) * 1.1F * p_212843_3_;
+        tail.xRot = -35.0F;
     }
 
     public void setupAnim(T fearwolf, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
