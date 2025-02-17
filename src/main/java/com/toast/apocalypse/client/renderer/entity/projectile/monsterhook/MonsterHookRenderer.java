f4c238762849d1565059ff8ec22e2ee9aee92141
Sarinsa <mikkemusbruker@gmail.com>
2025-02-17 18:37:44 +0100

    Improved rendering of grump monster hook

--- src/main/java/com/toast/apocalypse/client/renderer/entity/projectile/monsterhook/MonsterHookRenderer.java
+++ src/main/java/com/toast/apocalypse/client/renderer/entity/projectile/monsterhook/MonsterHookRenderer.java
@@ -8,19 +8,23 @@ import com.toast.apocalypse.common.entity.projectile.MonsterFishHook;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.culling.Frustum;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.FishingHookRenderer;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
+import net.minecraft.world.entity.FlyingMob;
 import net.minecraft.world.entity.HumanoidArm;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.phys.Vec3;
 import org.joml.Matrix3f;
 import org.joml.Matrix4f;
 
-/** Copied from {@link FishingHookRenderer} */
+/** Copied from SpecialMobs 'SpecialFishingBobberRenderer' */
 public class MonsterHookRenderer extends EntityRenderer<MonsterFishHook> {
 
     private static final ResourceLocation TEXTURE = Apocalypse.resourceLoc("textures/entity/projectile/monster_hook.png");
@@ -32,91 +36,121 @@ public class MonsterHookRenderer extends EntityRenderer<MonsterFishHook> {
     }
 
     @Override
-    public void render(MonsterFishHook fishHook, float p_225623_2_, float p_225623_3_, PoseStack poseStack, MultiBufferSource buffer, int p_225623_6_) {
-        LivingEntity livingEntity = fishHook.getLivingOwner();
+    public boolean shouldRender(MonsterFishHook fishHook, Frustum frustum, double cameraX, double cameraY, double cameraZ) {
+        return true;
+    }
 
-        if (livingEntity != null) {
-            poseStack.pushPose();
-            poseStack.pushPose();
-            poseStack.scale(0.5F, 0.5F, 0.5F);
-            poseStack.mulPose(this.entityRenderDispatcher.cameraOrientation());
-            poseStack.mulPose(Axis.YP.rotationDegrees(180.0F));
-            PoseStack.Pose pose = poseStack.last();
-            Matrix4f matrix4f = pose.pose();
-            Matrix3f matrix3f = pose.normal();
-            VertexConsumer vertexConsumer = buffer.getBuffer(RENDER_TYPE);
-            vertex(vertexConsumer, matrix4f, matrix3f, p_225623_6_, 0.0F, 0, 0, 1);
-            vertex(vertexConsumer, matrix4f, matrix3f, p_225623_6_, 1.0F, 0, 1, 1);
-            vertex(vertexConsumer, matrix4f, matrix3f, p_225623_6_, 1.0F, 1, 1, 0);
-            vertex(vertexConsumer, matrix4f, matrix3f, p_225623_6_, 0.0F, 1, 0, 0);
-            poseStack.popPose();
-            int i = livingEntity.getMainArm() == HumanoidArm.RIGHT ? 1 : -1;
+    @Override
+    public void render(MonsterFishHook entity, float rotation, float partialTicks,
+                       PoseStack poseStack, MultiBufferSource buffer, int packedLight) {
+        final LivingEntity angler = entity.getLivingOwner();
+        if (angler == null) return;
 
-            float f = livingEntity.getAttackAnim(p_225623_3_);
-            float f1 = Mth.sin(Mth.sqrt(f) * (float)Math.PI);
-            double d4;
-            double d5;
-            double d6;
-            float f3;
+        poseStack.pushPose();
 
-            if ((this.entityRenderDispatcher.options == null || this.entityRenderDispatcher.options.getCameraType().isFirstPerson()) && livingEntity == Minecraft.getInstance().player) {
-                double fov = this.entityRenderDispatcher.options.fov().get();
-                fov = fov / 100.0D;
-                Vec3 vec3 = new Vec3((double)i * -0.36D * fov, -0.045D * fov, 0.4D);
-                vec3 = vec3.xRot(-Mth.lerp(p_225623_3_, livingEntity.xRotO, livingEntity.getXRot()) * ((float)Math.PI / 180F));
-                vec3 = vec3.yRot(-Mth.lerp(p_225623_3_, livingEntity.yRotO, livingEntity.getYRot()) * ((float)Math.PI / 180F));
-                vec3 = vec3.yRot(f1 * 0.5F);
-                vec3 = vec3.xRot(-f1 * 0.7F);
-                d4 = Mth.lerp(p_225623_3_, livingEntity.xo, livingEntity.getX()) + vec3.x;
-                d5 = Mth.lerp(p_225623_3_, livingEntity.yo, livingEntity.getY()) + vec3.y;
-                d6 = Mth.lerp(p_225623_3_, livingEntity.zo, livingEntity.getZ()) + vec3.z;
-                f3 = livingEntity.getEyeHeight();
-            } else {
-                d4 = Mth.lerp(p_225623_3_, livingEntity.xo, livingEntity.getX());
-                d5 = livingEntity.yo + (double)livingEntity.getEyeHeight() + (livingEntity.getY() - livingEntity.yo) * (double)p_225623_3_ - 0.45D;
-                d6 = Mth.lerp(p_225623_3_, livingEntity.zo, livingEntity.getZ());
-                f3 = livingEntity.isCrouching() ? -0.1875F : 0.0F;
-            }
+        // Render the hook/bobber texture
+        poseStack.pushPose();
+        poseStack.scale(0.5F, 0.5F, 0.5F);
+        poseStack.mulPose(entityRenderDispatcher.cameraOrientation());
+        poseStack.mulPose(Axis.YP.rotationDegrees( 180.0F));
+        final PoseStack.Pose matrixEntry = poseStack.last();
+        drawQuad(buffer.getBuffer(RENDER_TYPE), matrixEntry.pose(), matrixEntry.normal(), packedLight);
+        poseStack.popPose();
 
-            double d9 = Mth.lerp(p_225623_3_, fishHook.xo, fishHook.getX());
-            double d10 = Mth.lerp(p_225623_3_, fishHook.yo, fishHook.getY()) + 0.25D;
-            double d8 = Mth.lerp(p_225623_3_, fishHook.zo, fishHook.getZ());
-            float f4 = (float)(d4 - d9);
-            float f5 = (float)(d5 - d10) + f3;
-            float f6 = (float)(d6 - d8);
-            VertexConsumer vertexConsumer1 = buffer.getBuffer(RenderType.lines());
-            PoseStack.Pose pose1 = poseStack.last();
-            int j = 16;
+        // Render the fishing line connecting the top of the bobber to the tip of angler's fishing rod
 
-            for(int k = 0; k < 16; ++k) {
-                stringVertex(f4, f5, f6, vertexConsumer1, pose1, fraction(k, 16), fraction(k + 1, 16));
+        // Define position of the fishing rod tip in local space
+        final ItemStack heldItem = angler.getMainHandItem();
+        // If main hand doesn't have a fishing rod, then we assume it's a mob that can't hold items
+        final int handedness = heldItem.getItem() != Items.FISHING_ROD && heldItem.getItem() != Items.STICK ? 0 :
+                angler.getMainArm() == HumanoidArm.RIGHT ? 1 : -1;
 
-            }
-            poseStack.popPose();
-            super.render(fishHook, p_225623_2_, p_225623_3_, poseStack, buffer, p_225623_6_);
+        final double forwardOffset = 0.5;
+        final double rightwardOffset = handedness * 0.35;
+        final double upwardOffset = angler.getEyeHeight() - (angler.isCrouching() ? 0.1875F : 0.0F);
+
+        double xRod, yRod, zRod;
+
+        // Assume flying anglers don't have arms or rods
+        if (angler instanceof FlyingMob) {
+            xRod = Mth.lerp(partialTicks, angler.xo, angler.getX());
+            yRod = Mth.lerp(partialTicks, angler.yo, angler.getY()) + upwardOffset;
+            zRod = Mth.lerp(partialTicks, angler.zo, angler.getZ());
+        }
+        else {
+            // Convert local space offsets to global space
+            final float yRot = Mth.lerp(partialTicks, angler.yBodyRotO, angler.yBodyRot) * (float) Math.PI / 180.0F;
+            final double forwardX = Mth.sin(yRot);
+            final double forwardZ = Mth.cos(yRot);
+
+            xRod = Mth.lerp(partialTicks, angler.xo, angler.getX()) - forwardX * forwardOffset - forwardZ * rightwardOffset;
+            yRod = Mth.lerp(partialTicks, angler.yo, angler.getY()) + upwardOffset;
+            zRod = Mth.lerp(partialTicks, angler.zo, angler.getZ()) + forwardZ * forwardOffset - forwardX * rightwardOffset;
+        }
+        final double xBobber = Mth.lerp(partialTicks, entity.xo, entity.getX());
+        final double yBobber = Mth.lerp(partialTicks, entity.yo, entity.getY()) + 0.25;
+        final double zBobber = Mth.lerp(partialTicks, entity.zo, entity.getZ());
+
+        drawLine(buffer.getBuffer(RenderType.lineStrip()), poseStack.last(), 16,
+                xBobber, yBobber, zBobber, xRod, yRod, zRod);
+
+        poseStack.popPose();
+
+        super.render(entity, rotation, partialTicks, poseStack, buffer, packedLight);
+    }
+
+    /** Creates the vertexes for a quad. */
+    private static void drawQuad(VertexConsumer vertexConsumer, Matrix4f pose, Matrix3f normal, int packedLight) {
+        quadVertex(vertexConsumer, pose, normal, packedLight, 0.0F, 0.0F, 0, 1);
+        quadVertex(vertexConsumer, pose, normal, packedLight, 1.0F, 0.0F, 1, 1);
+        quadVertex(vertexConsumer, pose, normal, packedLight, 1.0F, 1.0F, 1, 0);
+        quadVertex(vertexConsumer, pose, normal, packedLight, 0.0F, 1.0F, 0, 0);
+    }
+
+    private static void quadVertex(VertexConsumer vertexConsumer, Matrix4f pose, Matrix3f normal, int packedLight,
+                                    float dX, float dY, int u, int v) {
+        vertexConsumer.vertex(pose, dX - 0.5F, dY - 0.5F, 0.0F)
+                .color(255, 255, 255, 255) // RGBA - white is no tint
+                .uv(u, v).overlayCoords(OverlayTexture.NO_OVERLAY).uv2( packedLight)
+                .normal(normal, 0.0F, 1.0F, 0.0F).endVertex();
+    }
+
+    /** Creates the vertexes for the fishing line. A line from the bobber to the rod that hangs down a little in the y-axis. */
+    private static void drawLine(VertexConsumer vertexConsumer, PoseStack.Pose pose, int resolution,
+                                  double x1, double y1, double z1, double x2, double y2, double z2) {
+        final float dX = (float) (x2 - x1);
+        final float dY = (float) (y2 - y1);
+        final float dZ = (float) (z2 - z1);
+
+        for(int segment = 0; segment < resolution; segment++) {
+            // Each line segment is defined by 2 vertexes
+            lineVertex(dX, dY, dZ, vertexConsumer, pose, segment, resolution);
         }
     }
 
-    private static float fraction(int p_114691_, int p_114692_) {
-        return (float)p_114691_ / (float)p_114692_;
-    }
+    private static void lineVertex(float x, float y, float z, VertexConsumer vertexConsumer, PoseStack.Pose pose, float segment, float totalSegments) {
+        final float r = segment / totalSegments;
+        final float k = segment + 1 / totalSegments;
 
-    private static void vertex(VertexConsumer vertexConsumer, Matrix4f matrix4f, Matrix3f matrix3f, int p_114715_, float p_114716_, int p_114717_, int p_114718_, int p_114719_) {
-        vertexConsumer.vertex(matrix4f, p_114716_ - 0.5F, (float)p_114717_ - 0.5F, 0.0F).color(255, 255, 255, 255).uv((float)p_114718_, (float)p_114719_).overlayCoords(OverlayTexture.NO_OVERLAY).uv2(p_114715_).normal(matrix3f, 0.0F, 1.0F, 0.0F).endVertex();
-    }
+        float vertX = x * r;
+        float vertY = y * (r * r + r) * 0.5F + 0.25F;
+        float vertZ = z * r;
 
-    private static void stringVertex(float p_174119_, float p_174120_, float p_174121_, VertexConsumer vertexConsumer, PoseStack.Pose pose, float p_174124_, float p_174125_) {
-        float f = p_174119_ * p_174124_;
-        float f1 = p_174120_ * (p_174124_ * p_174124_ + p_174124_) * 0.5F + 0.25F;
-        float f2 = p_174121_ * p_174124_;
-        float f3 = p_174119_ * p_174125_ - f;
-        float f4 = p_174120_ * (p_174125_ * p_174125_ + p_174125_) * 0.5F + 0.25F - f1;
-        float f5 = p_174121_ * p_174125_ - f2;
-        float f6 = Mth.sqrt(f3 * f3 + f4 * f4 + f5 * f5);
-        f3 /= f6;
-        f4 /= f6;
-        f5 /= f6;
-        vertexConsumer.vertex(pose.pose(), f, f1, f2).color(0, 0, 0, 255).normal(pose.normal(), f3, f4, f5).endVertex();
+        float normalX = x * k - vertX;
+        float normalY = y * (k * k + k) * 0.5F + 0.25F - vertY;
+        float normalZ = z * k - vertZ;
+
+        float squared = Mth.sqrt(normalX * normalX + normalY * normalY + normalZ * normalZ);
+
+        normalX /= squared;
+        normalY /= squared;
+        normalZ /= squared;
+
+        vertexConsumer
+                .vertex(pose.pose(), vertX, vertY, vertZ)
+                .color(0, 0, 0, 255)
+                .normal(pose.normal(), normalX, normalY, normalZ)
+                .endVertex();
     }
 
     @Override
