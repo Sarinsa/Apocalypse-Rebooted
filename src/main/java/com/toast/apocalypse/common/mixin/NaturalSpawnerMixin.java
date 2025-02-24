66611d9b57eec508676a5940bac29e398e7fe12f
Sarinsa <mikkemusbruker@gmail.com>
2025-02-24 20:35:42 +0100

    lol

--- src/main/java/com/toast/apocalypse/common/mixin/NaturalSpawnerMixin.java
+++ src/main/java/com/toast/apocalypse/common/mixin/NaturalSpawnerMixin.java
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(NaturalSpawner.class)
-public class NaturalSpawnerMixin {
+public abstract class NaturalSpawnerMixin {
 
     /**
      * The purpose of this injector is to effectively skip checking
