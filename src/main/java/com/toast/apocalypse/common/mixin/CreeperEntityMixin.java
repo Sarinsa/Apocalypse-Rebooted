fbcb358299f984a173cd9cf1c6c96280366825ca
Sarinsa <mikkemusbruker@gmail.com>
2025-02-26 21:57:25 +0100

    Fixed CreeperEntityMixin not doing what it is supposed to

--- src/main/java/com/toast/apocalypse/common/mixin/CreeperEntityMixin.java
+++ src/main/java/com/toast/apocalypse/common/mixin/CreeperEntityMixin.java
@@ -1,8 +1,9 @@
 package com.toast.apocalypse.common.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import com.toast.apocalypse.common.misc.mixin_work.CommonMixinHooks;
+import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.AreaEffectCloud;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.PowerableMob;
 import net.minecraft.world.entity.monster.Creeper;
@@ -10,9 +11,13 @@ import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-//@Mixin(Creeper.class)
+import java.util.Collection;
+
+@Mixin(Creeper.class)
 public abstract class CreeperEntityMixin extends Monster implements PowerableMob {
 
     protected CreeperEntityMixin(EntityType<? extends Creeper> entityType, Level level) {
@@ -22,16 +27,16 @@ public abstract class CreeperEntityMixin extends Monster implements PowerableMob
     /**
      * A somewhat wonky solution to preventing
      * Creepers from spawning lingering effect
-     * clouds that can grant "infinite" buffs.
+     * clouds with infinite duration.
      */
-    /*
-    @Redirect(
+    @Inject(
             method = "spawnLingeringCloud",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;addFreshEntity(Lnet/minecraft/world/entity/Entity;)Z"))
-    public boolean redirectSpawnLingeringCloud(Level level, Entity entity) {
-        CommonMixinHooks.capAreaEffectCloudDurations((AreaEffectCloud) entity);
-        return level.addFreshEntity(entity);
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/level/Level;addFreshEntity(Lnet/minecraft/world/entity/Entity;)Z"
+            )
+    )
+    public void onSpawnLingeringCloud(CallbackInfo ci, @Local(ordinal = 0) AreaEffectCloud effectCloud) {
+        CommonMixinHooks.capLingeringCloudEffectDurations(effectCloud);
     }
-
-     */
 }
