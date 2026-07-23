38888dbe0a4b1815e6d435684a761b9d482e7393
Sarinsa <mikkemusbruker@gmail.com>
2026-07-23 23:30:31 +0200

    Cleaned up RainDamageTickHandler.java

--- src/main/java/com/toast/apocalypse/common/event/RainDamageTickHandler.java
+++ src/main/java/com/toast/apocalypse/common/event/RainDamageTickHandler.java
@@ -5,14 +5,13 @@ import com.toast.apocalypse.common.core.Apocalypse;
 import com.toast.apocalypse.common.misc.ApocalypseDamageSources;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.entity.EntityTypeTest;
 import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.server.ServerStartedEvent;
@@ -22,22 +21,31 @@ import net.minecraftforge.fml.common.Mod;
 import static com.toast.apocalypse.common.core.config.ApocalypseConfig.ACID_RAIN;
 
 // TODO - DON'T FORGET!!!!! Rain damage is currently only applied to entities in the overworld!
-//        And whats more, this thing needs cleanup in general, it looks terrible and horrible, and it looks terrible. It also looks horrible.
+
+/**
+ * This listener is responsible for calculating acid rain tick damage
+ * and dealing damage to entities that are exposed to the rain (or snow).
+ *
+ * @see com.toast.apocalypse.common.core.mod_event.EventRegistry#ACID_RAIN
+ */
 @Mod.EventBusSubscriber( modid = Apocalypse.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE )
-public class RainDamageTickHandler {
+public final class RainDamageTickHandler {
     
+    /** Whether acid snow is enabled. */
     private static boolean acidSnowEnabled = false;
-    private static int timeRainDmgCheck;
+    /** The amount of ticks until the next damage check should be performed. */
+    private static int timeNextDamageCheck;
     
     
-    private RainDamageTickHandler() { }
-    
+    /** Called when the server has finished loading and is ready to play. */
     @SubscribeEvent
     public static void onServerStarted( ServerStartedEvent event ) {
         acidSnowEnabled = ACID_RAIN.GENERAL.acidSnow.get();
     }
     
     /**
+     * Called each tick on the server.
+     * <br><br>
      * Checks if it is time to apply acid rain tick damage,
      * and applies damage to all exposed living entities (or only players depending on config).
      */
@@ -45,95 +53,57 @@ public class RainDamageTickHandler {
     public static void onServerTick( TickEvent.ServerTickEvent event ) {
         if( event.phase != TickEvent.Phase.END ) return;
         
-        boolean isRainingAcid = Apocalypse.INSTANCE.getDifficultyManager().isRainingAcid( event.getServer().overworld() );
+        final boolean isRainingAcid = Apocalypse.INSTANCE.getDifficultyManager().isRainingAcid( event.getServer().overworld() );
         
-        if( !isRainingAcid || ACID_RAIN.GENERAL.rainDamage.get() <= 0 )
-            return;
+        if( !isRainingAcid || ACID_RAIN.GENERAL.rainDamage.get() <= 0 ) return;
         
-        if( ++timeRainDmgCheck >= (ACID_RAIN.GENERAL.damageRate.get() * 20) ) {
-            ServerLevel overworld = event.getServer().overworld();
-            boolean playersOnly = !ACID_RAIN.GENERAL.damageMobs.get();
+        if( ++timeNextDamageCheck >= (ACID_RAIN.GENERAL.damageRate.get() * 20) ) {
+            timeNextDamageCheck = 0;
+            final ServerLevel overworld = event.getServer().overworld();
+            final boolean playersOnly = !ACID_RAIN.GENERAL.damageMobs.get();
             
-            if( playersOnly ) {
-                for( ServerPlayer player : overworld.players() ) {
-                    if( player.level().dimension() != Level.OVERWORLD ) continue;
-                    
-                    boolean rainingAcidAt = acidSnowEnabled
-                            ? isRainingOrSnowingAt( overworld, player.blockPosition().offset( 0, (int) player.getEyeHeight(), 0 ) )
-                            : overworld.isRainingAt( player.blockPosition().offset( 0, (int) player.getEyeHeight(), 0 ) );
-                    
-                    if( EnchantmentHelper.hasAquaAffinity( player ) || !rainingAcidAt )
-                        continue;
-                    
-                    ItemStack headStack = player.getItemBySlot( EquipmentSlot.HEAD );
-                    
-                    if( !headStack.isEmpty() ) {
-                        if( headStack.getItem() == ApocalypseObjects.Items.BUCKET_HELM.get() || headStack.getItem().getMaxDamage( headStack ) <= 0 ) {
-                            continue;
-                        }
-                        headStack.hurtAndBreak( player.getRandom().nextInt( 2 ), player, ( playerEntity ) -> player.broadcastBreakEvent( EquipmentSlot.HEAD ) );
-                    }
-                    else {
-                        player.hurt( ApocalypseDamageSources.of( overworld, ApocalypseDamageSources.ACID_RAIN ), ACID_RAIN.GENERAL.rainDamage.get() );
-                    }
-                }
-            }
-            else {
-                Iterable<Entity> allEntities = overworld.getAllEntities();
+            final Iterable<? extends LivingEntity> entities = playersOnly
+                    ? overworld.players()
+                    : overworld.getEntities( EntityTypeTest.forClass( LivingEntity.class ),
+                    ( entity ) -> !ACID_RAIN.GENERAL.mobBlacklist.contains( entity ) );
+            
+            for( LivingEntity entity : entities ) {
+                if( EnchantmentHelper.hasAquaAffinity( entity ) ) continue;
                 
-                for( Entity entity : allEntities ) {
-                    if( entity.level().dimension() != Level.OVERWORLD || ACID_RAIN.GENERAL.mobBlacklist.contains( entity ) )
+                boolean rainingAcidAt = acidSnowEnabled
+                        ? isRainingOrSnowingAt( overworld, entity.blockPosition().offset( 0, (int) entity.getEyeHeight(), 0 ) )
+                        : overworld.isRainingAt( entity.blockPosition().offset( 0, (int) entity.getEyeHeight(), 0 ) );
+                
+                if( !rainingAcidAt ) continue;
+                
+                final ItemStack headStack = entity.getItemBySlot( EquipmentSlot.HEAD );
+                
+                if( !headStack.isEmpty() ) {
+                    if( headStack.getItem() == ApocalypseObjects.Items.BUCKET_HELM.get() || headStack.getItem().getMaxDamage( headStack ) <= 0 ) {
                         continue;
-                    
-                    if( entity instanceof LivingEntity livingEntity ) {
-                        boolean rainingAcidAt = acidSnowEnabled
-                                ? isRainingOrSnowingAt( overworld, livingEntity.blockPosition().offset( 0, (int) livingEntity.getEyeHeight(), 0 ) )
-                                : overworld.isRainingAt( livingEntity.blockPosition().offset( 0, (int) livingEntity.getEyeHeight(), 0 ) );
-                        
-                        if( EnchantmentHelper.hasAquaAffinity( livingEntity ) || !rainingAcidAt )
-                            continue;
-                        
-                        ItemStack headStack = livingEntity.getItemBySlot( EquipmentSlot.HEAD );
-                        
-                        if( !headStack.isEmpty() ) {
-                            if( headStack.getItem() == ApocalypseObjects.Items.BUCKET_HELM.get() || headStack.getItem().getMaxDamage( headStack ) <= 0 ) {
-                                continue;
-                            }
-                            headStack.hurtAndBreak( livingEntity.getRandom().nextInt( 2 ), livingEntity, ( playerEntity ) -> livingEntity.broadcastBreakEvent( EquipmentSlot.HEAD ) );
-                        }
-                        else {
-                            livingEntity.hurt( ApocalypseDamageSources.of( overworld, ApocalypseDamageSources.ACID_RAIN ), ACID_RAIN.GENERAL.rainDamage.get() );
-                        }
                     }
+                    // TODO configurable damage to the equipped head stack
+                    headStack.hurtAndBreak( entity.getRandom().nextInt( 2 ), entity, ( playerEntity ) -> entity.broadcastBreakEvent( EquipmentSlot.HEAD ) );
+                }
+                else {
+                    entity.hurt( ApocalypseDamageSources.of( overworld, ApocalypseDamageSources.ACID_RAIN ), ACID_RAIN.GENERAL.rainDamage.get() );
                 }
             }
-            resetTimer();
         }
     }
     
-    /**
-     * @return True if it is currently raining or snowing at the given block position.
-     */
+    /** @return True if it is currently raining or snowing at the given block position. */
     public static boolean isRainingOrSnowingAt( Level level, BlockPos pos ) {
-        if( !level.isRaining() ) {
-            return false;
-        }
-        else if( !level.canSeeSky( pos ) ) {
-            return false;
-        }
-        else if( level.getHeightmapPos( Heightmap.Types.MOTION_BLOCKING, pos ).getY() > pos.getY() ) {
-            return false;
-        }
+        if( !level.isRaining() ) return false;
+        if( !level.canSeeSky( pos ) ) return false;
+        if( level.getHeightmapPos( Heightmap.Types.MOTION_BLOCKING, pos ).getY() > pos.getY() ) return false;
         else {
             Biome biome = level.getBiome( pos ).value();
             return biome.getPrecipitationAt( pos ) == Biome.Precipitation.RAIN || biome.getPrecipitationAt( pos ) == Biome.Precipitation.SNOW;
         }
     }
     
-    /**
-     * Call to reset damage tick timer.
-     */
-    public static void resetTimer() {
-        timeRainDmgCheck = 0;
-    }
+    
+    // Utility class
+    private RainDamageTickHandler() { }
 }
