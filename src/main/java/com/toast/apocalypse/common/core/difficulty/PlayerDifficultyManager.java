8b82bf1eb6eb6509931fbc1fc5e74dafc9c1e6ec
Sarinsa <mikkemusbruker@gmail.com>
2026-09-13 13:38:25 +0200

    More private, but also more access. How can this be?

--- src/main/java/com/toast/apocalypse/common/core/difficulty/PlayerDifficultyManager.java
+++ src/main/java/com/toast/apocalypse/common/core/difficulty/PlayerDifficultyManager.java
@@ -92,7 +92,7 @@ public final class PlayerDifficultyManager {
     private boolean serverStopped = false;
     
     
-    public PlayerDifficultyManager() {}
+    public PlayerDifficultyManager() { }
     
     
     /** @return The current time of day in the given level. */
@@ -446,7 +446,7 @@ public final class PlayerDifficultyManager {
         }
         
         // Check for events to start
-        for( EventType<?> type : EventRegistry.EVENTS.values() ) {
+        for( EventType<?> type : EventRegistry.allTypes() ) {
             if( !events.containsKey( type ) ) {
                 IEventPredicate startPredicate = type.getStartPredicate();
                 
