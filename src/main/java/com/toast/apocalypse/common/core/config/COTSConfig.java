f38040b6b4e07740d045d1c70ed5f9f15472223c
Sarinsa <mikkemusbruker@gmail.com>
2026-09-14 01:16:48 +0200

    More configurability for the darkness event

--- src/main/java/com/toast/apocalypse/common/core/config/COTSConfig.java
+++ src/main/java/com/toast/apocalypse/common/core/config/COTSConfig.java
@@ -1,10 +1,18 @@
 package com.toast.apocalypse.common.core.config;
 
+import com.toast.apocalypse.api.lib.ApocalypseObjects;
 import fathertoast.crust.api.config.common.AbstractConfigCategory;
 import fathertoast.crust.api.config.common.AbstractConfigFile;
 import fathertoast.crust.api.config.common.ConfigManager;
 import fathertoast.crust.api.config.common.field.BooleanField;
+import fathertoast.crust.api.config.common.field.EnvironmentListField;
 import fathertoast.crust.api.config.common.field.IntField;
+import fathertoast.crust.api.config.common.field.collection.RegistryWeightedListField;
+import fathertoast.crust.api.config.common.value.collection.RegistryWeightedList;
+import fathertoast.crust.api.config.common.value.collection.value.BooleanValueCodec;
+import fathertoast.crust.api.config.common.value.environment.EnvironmentList;
+import net.minecraft.world.entity.EntityType;
+import net.minecraftforge.registries.ForgeRegistries;
 
 public class COTSConfig extends AbstractConfigFile {
     
@@ -13,17 +21,23 @@ public class COTSConfig extends AbstractConfigFile {
     /** Builds the config spec that should be used for this config. */
     public COTSConfig( ConfigManager cfgManager, String cfgName ) {
         super( cfgManager, cfgName, false,
-                "This config contains settings related to Apocalypse's 'Call of The Shadows event'."
+                "This config contains settings related to Apocalypse's 'Call of The Shadows' event."
         );
         GENERAL = new COTSConfig.General( this );
+        
+        SPEC.fileOnlyNewLine();
+        EnvironmentListField.describe1of2( SPEC );
+        EnvironmentListField.describe2of2( SPEC );
     }
     
     
     public static class General extends AbstractConfigCategory<COTSConfig> {
         
         public final BooleanField enabled;
-        public final IntField blockLightLevel;
-        public final IntField skyLightLevel;
+        public final IntField.RandomRange blindnessTicks;
+        
+        public final EnvironmentListField<Boolean> conditions;
+        public final RegistryWeightedListField<EntityType<?>> spawnList;
         
         General( COTSConfig parent ) {
             super( parent, "general",
@@ -31,14 +45,36 @@ public class COTSConfig extends AbstractConfigFile {
             
             enabled = SPEC.define( new BooleanField( "enabled", true,
                     "If true, this event is enabled and will trigger if the circumstances are correct. See below settings." ) );
-            blockLightLevel = SPEC.define( new IntField( "block_light_level", 0, 0, 15,
-                    "The maximum required block light level for this event to start.",
-                    "Block light is the light that comes from sources that are not skylight, such as torches or glowstone.",
-                    "NOTE: If RyoamicLights is installed, all dynamic light sources will be considered block light for this event." ) );
-            skyLightLevel = SPEC.define( new IntField( "sky_light_level", 0, 0, 15,
-                    "The maximum required sky light level for this event to start.",
-                    "Sky light is the light that comes from the sky (no way!). Take note that sky light does not decrease if it is night, it only decreases" +
-                            " when a solid blocks obstruct view to the sky." ) );
+            
+            SPEC.newLine();
+            
+            blindnessTicks = new IntField.RandomRange( SPEC, "blindness_ticks", 40, 60, IntField.Range.TOKEN_NEGATIVE,
+                    "The min and max (inclusive) duration in ticks for the blindness potion effect that is applied to players when this event spawns a mob." );
+            
+            SPEC.newLine();
+            
+            conditions = SPEC.define( new EnvironmentListField<>( "conditions", createDefaultConditions(),
+                    "A list of environment conditions that must be met for this event to start.",
+                    "By default, the event only triggers in complete darkness, and not in The End or Nether." ) );
+            spawnList = SPEC.define( new RegistryWeightedListField<>( "spawn_list", createDefaultSpawnList(),
+                    "A weighted list of mobs that can be spawned by this event.",
+                    "Leaving this list empty will effectively disable this event." ) );
+        }
+        
+        private static EnvironmentList<Boolean> createDefaultConditions() {
+            return EnvironmentList.builder( BooleanValueCodec.DEFAULT_FALSE )
+                    .entryBuilder( true )
+                    .atBrightness( 0 ).and()
+                    .notInTheEnd().and()
+                    .notInNether()
+                    .build().build();
+        }
+        
+        private static RegistryWeightedList<EntityType<?>> createDefaultSpawnList() {
+            return new RegistryWeightedList.Builder<>( ForgeRegistries.ENTITY_TYPES )
+                    .add( 100, ApocalypseObjects.EntityTypes.SHADEFIEND )
+                    .add( 10, ApocalypseObjects.EntityTypes.GHOST )
+                    .build();
         }
     }
 }
